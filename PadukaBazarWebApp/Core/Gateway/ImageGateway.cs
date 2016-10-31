ee2ca5e94195ce4b53130089ff97c861397dc082
Mustak Ahmed <kpimustak@gmail.com>
2016-11-01 01:03:40 +0600

    ImageModelInclude
    Image Model Include first time

---
+++ PadukaBazarWebApp/Core/Gateway/ImageGateway.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PadukaBazarWebApp.Models;
+
+namespace PadukaBazarWebApp.Core.Gateway
+{
+    public class ImageGateway : Gateway
+    {
+        public int InsertImage(Image image)
+        {
+            Query = "NSERT INTO Image VALUES('" + image.ImagePath + "'," + image.IemId + ")";
+            Command.CommandText = Query;
+            Connection.Open();
+            int rowAffected = Command.ExecuteNonQuery();
+            Connection.Close();
+            return rowAffected;
+        }
+
+        public List<Image> GetImageList()
+        {
+            List<Image> images = new List<Image>();
+            Query = "SELECT * FROM Image";
+            Command.CommandText = Query;
+            Connection.Open();
+            Reader = Command.ExecuteReader();
+            while (Reader.Read())
+            {
+                Image image = new Image();
+                image.Id = Convert.ToInt32(Reader["Id"]);
+                image.ImagePath = Reader["ImagePath"].ToString();
+                image.IemId = Convert.ToInt32(Reader["ItemId"]);
+
+                images.Add(image);
+            }
+            Reader.Close();
+            Connection.Close();
+            return images;
+        }
+    }
+}
---
+++ PadukaBazarWebApp/Core/Manager/ImageManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PadukaBazarWebApp.Core.Gateway;
+using PadukaBazarWebApp.Models;
+
+namespace PadukaBazarWebApp.Core.Manager
+{
+    public class ImageManager
+    {
+        ImageGateway imageGateway = new ImageGateway();
+
+        public string InsertItem(Image image)
+        {
+            int rowAffaced = imageGateway.InsertImage(image);
+            if (rowAffaced != 0)
+            {
+                return "Save Successfull.";
+            }
+            else
+            {
+                return "Sory! Save Fail.";
+            }
+
+        }
+        
+    }
+}
