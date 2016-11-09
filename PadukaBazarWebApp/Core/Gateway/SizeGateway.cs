0a4be1a9ee43f8542de5f9def9d305d351629644
Abdullah Al Noman <diu.noman@gmail.com>
2016-11-09 19:27:15 +0600

    SizeGateway
    from class lab

---
+++ PadukaBazarWebApp/Core/Gateway/SizeGateway.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PadukaBazarWebApp.Models;
+namespace PadukaBazarWebApp.Core.Gateway
+{
+    public class SizeGateway:Gateway
+    {
+
+        public int  InsertSize(Size size)
+        {
+            Query = "insert into Size values('"+size.ItemId+"','"+size.ItemSize+"','"+size.Quentity+"')";
+
+
+            Command.CommandText = Query;
+            Connection.Open();
+            int rowAffected = Command.ExecuteNonQuery();
+            Connection.Close();
+            return rowAffected;
+
+        }
+
+
+
+        public List<Size> GetAllSize()
+        {
+
+            List<Size> sizes = new List<Size>();
+
+
+
+
+
+
+            return sizes;
+        }
+
+
+    }
+}
