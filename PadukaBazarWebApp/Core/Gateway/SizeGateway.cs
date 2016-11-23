6da3a87caf996aa633169a63f3e651439375504e
Md. Shahinul Islam <shahinul.cse@gmail.com>
2016-11-23 20:05:18 +0600

    Size Gateway change GetSizeList.................

--- PadukaBazarWebApp/Core/Gateway/SizeGateway.cs
+++ PadukaBazarWebApp/Core/Gateway/SizeGateway.cs
@@ -11,31 +11,27 @@ namespace PadukaBazarWebApp.Core.Gateway
         public int  InsertSize(Size size)
         {
             Query = "insert into Size values('"+size.ItemId+"','"+size.ItemSize+"','"+size.Quentity+"')";
-
-
             Command.CommandText = Query;
             Connection.Open();
             int rowAffected = Command.ExecuteNonQuery();
             Connection.Close();
             return rowAffected;
-
         }
-
-
-
-        public List<Size> GetAllSize()
+        public List<Size> GetSizeList()
         {
-
             List<Size> sizes = new List<Size>();
-
-
-
-
-
-
+            while (Reader.Read())
+            {
+                Size size = new Size();
+                size.Id = Convert.ToInt32(Reader["Id"]);
+                size.ItemSize = Convert.ToInt32(Reader["ItemSize"]);
+                size.Quentity = Convert.ToInt32(Reader["Quentity"]);
+                size.ItemId = Convert.ToInt32(Reader["ItemId"]);
+                size.Add(size);
+            }
+            Reader.Close();
+            Connection.Close();
             return sizes;
         }
-
-
     }
 }
